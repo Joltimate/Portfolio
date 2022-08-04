f14e8024b9a15154729eccb580cddff7bc4f07aa
Joltimate <jultimate69@gmail.com>
2022-08-04 14:32:46 -0400

    updates

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import MECrop from '../../assets/MECrop.jpg'
+import HeaderSocials from './HeaderSocials'
+
 const Header = () => {
   return (
     <header>
@@ -9,6 +12,13 @@ const Header = () => {
       <h1>Jose Nunez</h1>
       <h5 className=".text-light">Junior Software Developer</h5>
       <CTA/>
+      <HeaderSocials/>
+      <div className='me'>
+        <img src={MECrop} alt=""/>
+
+      </div>
+
+      <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
---
+++ react-portfolio/src/components/header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {AiFillLinkedin} from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+import {AiFillLinkedin}
+        <a href='https://www.linkedin.com/in/jose-nunez-805a3220a/' target="_blank"><AiFillLinkedin/></a>
+        <a href='https://github.com/Joltimate' target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
