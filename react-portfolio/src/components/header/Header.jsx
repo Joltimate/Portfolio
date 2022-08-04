7ed0de466ffd41c6b8535df131bbc5550a2c01a6
Joltimate <jultimate69@gmail.com>
2022-08-04 13:41:11 -0400

    Added Buttons

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './header.css'
-
+import CTA from './CTA'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className =".container.header__container">
+      <h5>Hello I'm</h5>
+      <h1>Jose Nunez</h1>
+      <h5 className=".text-light">Junior Software Developer</h5>
+      <CTA/>
+      </div>
+    </header>
   )
 }
 
