bd0016462add79254e974d2b27a9fd8c3dc42bf8
Joltimate <jultimate69@gmail.com>
2022-08-04 14:52:07 -0400

    added github icon

--- react-portfolio/src/components/header/HeaderSocials.jsx
+++ react-portfolio/src/components/header/HeaderSocials.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href='https://www.linkedin.com/in/jose-nunez-805a3220a/' target="_blank"><BsLinkedin/></a>
-        <a href='https://github.com/Joltimate' target="_blank"></a>
+        <a href='https://github.com/Joltimate' target="_blank"><FaGithub/></a>
     </div>
   )
 }
