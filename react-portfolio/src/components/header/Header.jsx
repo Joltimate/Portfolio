f43a9b6f3f40f2637295ad04ba6a460bc8fd77ac
Joltimate <jultimate69@gmail.com>
2022-08-05 15:04:56 -0400

    Header changes were made

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -7,15 +7,15 @@ import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
     <header>
-      <div className =".container.header__container">
+      <div className ="container header__container">
       <h5>Hello I'm</h5>
       <h1>Jose Nunez</h1>
       <h5 className=".text-light">Junior Software Developer</h5>
       <CTA/>
       <HeaderSocials/>
-      <div className='me'>
-        <img src={MECrop} alt=""/>
 
+      <div>
+        <img src={MECrop} alt="" className='me'/>
       </div>
 
       <a href='#contact' className='scroll__down'>Scroll Down</a>
