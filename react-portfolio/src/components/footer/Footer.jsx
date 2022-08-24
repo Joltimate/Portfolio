b687ecad479c1b80584caad7119d5cb0cc48d3e2
Joltimate <jultimate69@gmail.com>
2022-08-24 19:34:58 -0400

    Changed my name

--- react-portfolio/src/components/footer/Footer.jsx
+++ react-portfolio/src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ import {FaGithub} from 'react-icons/fa'
 const Footer = () => {
   return (
     <footer>
-      <a href= "#" className='footer__logo'>Joltimate</a>
+      <a href= "#" className='footer__logo'>Jose Nunez</a>
 
       <ul className="permalinks">
         <li><a href="#">Home</a></li>
